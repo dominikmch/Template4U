85a919082bb44970a406968d582e9e175598b046
Alexander Longhurst <P17185003@my365.dmu.ac.uk>
2020-02-10 15:52:35 +0000

    Added PropertyOK tests to each property

--- Template4UTesting/tstStaff.cs
+++ Template4UTesting/tstStaff.cs
@@ -11,3 +11,55 @@
     Assert.IsNotNull(AStaff);
 	}
 
+[TestMethod]
+
+public StaffIDPropertyOK()
+{
+    clsStaff AStaff = new clsStaff();
+    Int32 TestData = 1;
+    AStaff.StaffID = TestData;
+    Assert.AreEqual(AStaff.StaffID, TestData);
+}
+
+[TestMethod]
+
+public StaffRolePropertyOK()
+{
+    clsStaff AStaff = new clsStaff();
+    string TestData = "Manager";
+    AStaff.StaffRole = TestData;
+    Assert.AreEqual(AStaff.StaffID, TestData);
+}
+
+[TestMethod]
+
+public StartingDatePropertyOK()
+{
+    clsStaff AStaff = new clsStaff;
+    DateTime TestData = DateTime.Now.Date;
+    AStaff.StartingDate = TestData;
+    Assert.AreEqual(AStaff.StartingDate, TestData);
+}
+
+[TestMethod]
+
+public SalaryPropertyOK()
+{
+    clsStaff AStaff = new clsStaff;
+    double TestData = 0;
+    AStaff.Salary = TestData;
+    Assert.AreEqual(AStaff.Salary, TestData);
+}
+
+[TestMethod]
+
+public isEmployedPropertyOK()
+{
+    clsStaff AStaff = new clsStaff;
+    Boolean TestData = true;
+    AStaff.isEmployed = TestData;
+    Assert.AreEqual(AStaff.isEmployed, TestData);
+}
+
+
+
