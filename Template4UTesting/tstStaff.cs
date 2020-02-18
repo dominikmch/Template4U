25df0a1b2589284bd22d1c0951acff8df0c406e3
Alexander Longhurst <P17185003@my365.dmu.ac.uk>
2020-02-18 12:22:17 +0000

    fixing

---
+++ Template4UTesting/tstStaff.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Template4UTesting
+{
+    [TestClass]
+    public class tstStaff
+    {
+
+
+        [TestMethod]
+
+        public InstanceOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            Assert.IsNotNull(AStaff);
+        }
+
+        [TestMethod]
+
+        public StaffIDPropertyOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            Int32 TestData = 1;
+            AStaff.StaffID = TestData;
+            Assert.AreEqual(AStaff.StaffID, TestData);
+        }
+
+        [TestMethod]
+
+        public StaffRolePropertyOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            string TestData = "Manager";
+            AStaff.StaffRole = TestData;
+            Assert.AreEqual(AStaff.StaffID, TestData);
+        }
+
+        [TestMethod]
+
+        public StartingDatePropertyOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            DateTime TestData = DateTime.Now.Date;
+            AStaff.StartingDate = TestData;
+            Assert.AreEqual(AStaff.StartingDate, TestData);
+        }
+
+        [TestMethod]
+
+        public SalaryPropertyOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            double TestData = 0;
+            AStaff.Salary = TestData;
+            Assert.AreEqual(AStaff.Salary, TestData);
+        }
+
+        [TestMethod]
+
+        public isEmployedPropertyOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            Boolean TestData = true;
+            AStaff.isEmployed = TestData;
+            Assert.AreEqual(AStaff.isEmployed, TestData);
+        }
+
+
+    }
+
+}
+
