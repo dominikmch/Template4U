83b467b6b59972bf4d785c15f527b9736284f281
Dominik Michalski <55097755+dominikmch@users.noreply.github.com>
2020-03-03 14:48:51 +0000

    fixing

---
+++ Template4UClassLib/clsReview.cs
@@ -0,0 +1,6 @@
+﻿namespace Template4UBackend
+{
+    public class clsReview
+    {
+    }
+}
---
+++ Template4UTesting/tstReview.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Template4UBackend;
+
+namespace Template4UTesting
+{
+    [TestClass]
+    public class tstReview
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsReview Review = new clsReview();
+            Assert.IsNotNull(Review);
+        }
+    }
+
+    
+}
