8bc5595a9f07e98a7756443be5a2d60feb9139df
Dominik Michalski <55097755+dominikmch@users.noreply.github.com>
2020-03-04 12:49:39 +0000

    Test methods for the Review class

--- Template4UClassLib/clsReview.cs
+++ Template4UClassLib/clsReview.cs
@@ -1,6 +1,13 @@
-﻿namespace Template4UClassLib
+﻿using System;
+
+namespace Template4UClassLib
 {
     public class clsReview
     {
+        public bool Edited { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int Grade { get; set; }
+        public string Description { get; set; }
+        public int ReviewId { get; set; }
     }
 }
--- Template4UTesting/tstReview.cs
+++ Template4UTesting/tstReview.cs
@@ -15,6 +15,55 @@ namespace Template4UTesting
             clsReview Review = new clsReview();
             Assert.IsNotNull(Review);
         }
+
+    [TestMethod]
+    public void EditedPropertyOK()
+        {
+            clsReview Review = new clsReview();
+            Boolean TestData = true;
+            Review.Edited = TestData;
+            Assert.AreEqual(Review.Edited, TestData);
+        }
+
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            clsReview Review = new clsReview();
+
+            DateTime TestData = DateTime.Now.Date;
+            Review.DateAdded = TestData;
+            Assert.AreEqual(Review.DateAdded, TestData);
+        }
+
+        [TestMethod]
+        public void GradePropertyOK()
+        {
+            clsReview Review = new clsReview();
+
+            int TestData = 4;
+            Review.Grade = TestData;
+            Assert.AreEqual(Review.Grade, TestData);
+        }
+
+        [TestMethod]
+        public void DescriptionPropertyOK()
+        {
+            clsReview Review = new clsReview();
+
+            String TestData = "Perfect Widget, good customer service";
+            Review.Description = TestData;
+            Assert.AreEqual(Review.Description, TestData);
+        }
+
+        [TestMethod]
+        public void ReviewIdPropertyOK()
+        {
+            clsReview Review = new clsReview();
+
+            int TestData = 1;
+            Review.ReviewId = TestData;
+            Assert.AreEqual(Review.ReviewId, TestData);
+        }
     }
 
     
