ba9de59cd2b0636ae1ba8037573be09bfee19354
Jakub Zarzycki <P2503108@my365.dmu.ac.uk>
2020-03-10 12:31:26 +0000

    created clsOrder

---
+++ Template4UClassLib/clsOrder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Template4UClassLib
+{
+    public class clsOrder
+    {
+        private int mOrderId;
+        private string mPromoCode;
+        private DateTime mDatePlaced;
+        private Boolean mIsCompleted;
+        public int OrderId
+        {
+            get
+            {
+                return mOrderId;
+            }
+            set
+            {
+                mOrderId = value;
+            }
+        }
+
+        public DateTime DatePlaced
+        {
+            get
+            {
+                return mDatePlaced;
+            }
+            set
+            {
+                mDatePlaced = value;
+            }
+        }
+        public bool IsCompleted
+        {
+            get
+            {
+                return mIsCompleted;
+            }
+            set
+            {
+                mIsCompleted = value;
+            }
+        }
+
+    }
+}
---
+++ Template4UTesting/tstOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Template4UClassLib;
+
+namespace Template4UTesting
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+             
+
+            clsOrder Order = new clsOrder();
+            Assert.IsNotNull(Order);
+        }
+    }
+}
