5deeaeb4bddef611f1bd75781d1df7c630c6451e
Zawies <zar.kuba@gmail.com>
2020-03-16 21:49:11 +0000

    added find method and more tests

--- Template4UClassLib/clsOrder.cs
+++ Template4UClassLib/clsOrder.cs
@@ -4,6 +4,7 @@ namespace Template4UClassLib
     public class clsOrder
     {
         private int mOrderId;
+        private int mOrderlineId;
         private string mPromoCode;
         private DateTime mDatePlaced;
         private Boolean mIsCompleted;
@@ -16,6 +17,17 @@ namespace Template4UClassLib
             set
             {
                 mOrderId = value;
+            }
+        }
+        public int OrderlineId
+        {
+            get
+            {
+                return mOrderlineId;
+            }
+            set
+            {
+                mOrderlineId = value;
             }
         }
 
@@ -51,6 +63,28 @@ namespace Template4UClassLib
             {
                 mPromoCode = value;
             }
+        }
+
+        public bool find(int ReviewId)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@OrderId", OrderId);
+            DB.Execute("sproc_tblOrder_SelectById");
+            if (DB.Count == 1)
+            {
+                mOrderId = Convert.ToInt32(DB.DataTable.Rows[0]["OrderId"]);
+                mOrderlineId = Convert.ToInt32(DB.DataTable.Rows[0]["OrderlineId"]);
+                mIsCompleted = Convert.ToBoolean(DB.DataTable.Rows[0]["IsCompleted"]);
+                mPromoCode = Convert.ToString(DB.DataTable.Rows[0]["PromoCode"]);
+                mDatePlaced= Convert.ToDateTime(DB.DataTable.Rows[0]["DatePlaced"]);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
 
     }
--- Template4UTesting/tstOrder.cs
+++ Template4UTesting/tstOrder.cs
@@ -45,6 +45,105 @@ namespace Template4UTesting
             Order.IsCompleted = TestData;
             Assert.AreEqual(Order.IsCompleted, TestData);
         }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            clsOrder Order = new clsOrder();
+
+            Boolean Found = false;
+            Found = Order.findAll();
+            Assert.IsTrue(Found);
+        }
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            clsOrder Order = new clsOrder();
+
+            Boolean Found = false;
+            int OrderId = 2;
+            Found = Order.find(OrderId);
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestReviewFound()
+        {
+            clsOrder Order = new clsOrder();
+
+            Boolean Found = false;
+            Boolean OK = true;
+            int OrderId = 2;
+            Found = Order.find(OrderId);
+            if (Order.OrderId != 2)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestPromoCodeFound()
+        {
+            clsOrder Order = new clsOrder();
+
+            Boolean Found = false;
+            Boolean OK = true;
+            int OrderId = 2;
+            Found = Order.find(OrderId);
+            if (Order.PromoCode != "VALENTINES")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestOrderlineIdFound()
+        {
+            clsOrder Order = new clsOrder();
+
+            Boolean Found = false;
+            Boolean OK = true;
+            int OrderId = 2;
+            Found = Order.find(OrderId);
+            if (Order.OrderlineId != 2)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestIsCompletedFound()
+        {
+            clsOrder Order = new clsOrder();
+
+            Boolean Found = false;
+            Boolean OK = true;
+            int OrderId = 2;
+            Found = Order.find(OrderId);
+            if (!Order.IsCompleted)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestDatePlacedFound()
+        {
+
+            clsOrder Order = new clsOrder();
+
+            Boolean Found = false;
+            Boolean OK = true;
+            int OrderId = 2;
+            Found = Order.find(OrderId);
+            if (Order.DatePlaced != Convert.ToDateTime("10/03/2020"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+
     }
 
 }
