fc6d3f1f69fa79d090d594ff2bc48b6d6697651b
Dawid Malecki <P2429951@my365.dmu.ac.uk>
2020-05-15 19:33:49 +0100

    test

---
+++ Template4UTesting/tstProductList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Template4UClassLib;
+
+
+namespace Template4UTesting
+{
+    [TestClass]
+    public class tstProductList
+    {
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsProductList Product = new clsProductList();
+            Assert.IsNotNull(Product);
+        }
+
+    }
+}
+
